a06816e87000157a5a1f80e1be526331ffc251b6
OhhInk <oscarohhink@gmail.com>
2020-06-06 08:42:09 +0800

    seeder fix

--- src/database/seeds/AdminSeeder.php
+++ src/database/seeds/AdminSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-class UserSeeder extends Seeder
+class AdminSeeder extends Seeder
 {
     /**
      * Run the database seeds.
